81bbbebf859e0f4954e34a574f00cd9dc62a12e2
lalyos <lalyos@yahoo.com>
2019-04-04 08:24:54 +0000

    initial version

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat > /var/www/html/index.nginx-debian.html <<EOF
+<html>
+<body bgcolor="$COLOR">
+<h1>$TITLE</h1>
+
+$BODY
+
+<hr/>
+hostname: $HOSTNAME
+</body>
+</html>
+EOF
+
+
+nginx -g "daemon off;"
