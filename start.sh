lalyos/docker-12factor

add COLOR2 and BODYSTYLE env vars

start.sh
@@ -3,9 +3,15 @@
 
 echo -n "${FAIL:=0}" > /tmp/fail
 chown nginx:nginx /tmp/fail
+
+if [[ ! "$BODYSTYLE" ]]; then 
+  BODYSTYLE="background: ${COLOR}"
+  [ -z $COLOR2 ] || BODYSTLE="background: linear-gradient(${COLOR}, 80%, ${COLOR2})"
+fi
+
 cat > /usr/share/nginx/html/index.html <<EOF
 <html>
-<body bgcolor="$COLOR">
+<body style="${BODYSTYLE};">
 <h1>$TITLE</h1>
 
 $BODY

piitaj/gimlet-sample-app

start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 [[ "$COLOR" =~ ^(rnd|random|RND|RANDOM)$ ]] && COLOR="#$(printf '%03x' $(($RANDOM % 4096)))"
+
+if [[ ! "$BODYSTYLE" ]]; then 
+  BODYSTYLE="background: ${COLOR}"
+  [ -z $COLOR2 ] || BODYSTYLE="background: linear-gradient(${COLOR}, 80%, ${COLOR2})"
+fi
+
 cat > /var/www/html/index.nginx-debian.html <<EOF
 <html>
-<body bgcolor="$COLOR">
+<body style="${BODYSTYLE};">
 <h1>$TITLE</h1>
 
 $BODY
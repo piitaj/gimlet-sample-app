8a569286b54935745883da9fdc9b83425fb2b33c
lalyos <lalyos@yahoo.com>
2022-10-11 14:41:01 +0200

    add COLOR2 and BODYSTYLE env vars

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 [[ "$COLOR" =~ ^(rnd|random|RND|RANDOM)$ ]] && COLOR="#$(printf '%03x' $(($RANDOM % 4096)))"
+
+if [[ ! "$BODYSTYLE" ]]; then 
+  BODYSTYLE="background: ${COLOR}"
+  [ -z $COLOR2 ] || BODYSTYLE="background: linear-gradient(${COLOR}, 80%, ${COLOR2})"
+fi
+
 cat > /var/www/html/index.nginx-debian.html <<EOF
 <html>
-<body bgcolor="$COLOR">
+<body style="${BODYSTYLE};">
 <h1>$TITLE</h1>
 
 $BODY
